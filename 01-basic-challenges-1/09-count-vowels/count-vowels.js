91f7fd003576c04faedb7336c78171217999ffeb
KQuiggins <kashfamilyvalues@gmail.com>
2023-09-09 09:12:03 -0400

    finished all basic challenges 1-10

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,20 @@
-function countVowels() {}
+// function countVowels(str) {
+//     const vowelsOnly = str.replace(/[^aeiouAEIOU]/g, '');
+
+//     return vowelsOnly.length;
+// };
+
+function countVowels(str) {
+    let vowels = 'aeiouAEIOU';
+    let count = 0;
+
+   for (let i = 0; i < str.length; i++){
+      if (vowels.includes(str[i])) {
+          count++
+      }
+};
+
+    return count;
+};
 
 module.exports = countVowels;
--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,19 @@
-function removeDuplicates() {}
+// function removeDuplicates(arr) {
+//    const noDuplicates = [];
+
+//    for (let idx = 0; idx < arr.length; idx++) {
+//     if(!noDuplicates.includes(arr[idx]))
+//         noDuplicates.push(arr[idx]);
+//    }
+
+//    return noDuplicates;
+// }
+
+
+function removeDuplicates(arr) {
+
+    return Array.from(new Set(arr));
+}
+
 
 module.exports = removeDuplicates;
