cb3bac9b45da78d73ac00ea25fc6b36f6463e65f
KQuiggins <kashfamilyvalues@gmail.com>
2023-09-11 13:08:52 -0400

    completed fizzbuzz & array intersection challenges

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,18 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(num) {
+    const arr = [];
+
+    for (let idx = 1; idx <= num; idx++) {
+        if (idx % 3 === 0 && idx % 5 === 0) {
+            arr.push("FizzBuzz");
+        } else if (idx % 3 === 0) {
+            arr.push("Fizz");
+        } else if (idx % 5 === 0) {
+            arr.push("Buzz")
+        } else {
+            arr.push(idx);
+        }
+    }
+    return arr;
+};
 
 module.exports = fizzBuzzArray;
--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,28 @@
-function arrayIntersection() {}
+// function arrayIntersection(arr1, arr2) {
+//     const commonNumbers = [];
+
+//     for (let idx = 0; idx < arr1.length; idx++) {
+//         if (arr2.includes(arr1[idx]) && !commonNumbers.includes(arr1[idx])) {
+//             commonNumbers.push(arr1[idx]);
+//         }
+
+//     }
+
+//     return commonNumbers;
+// };
+
+// Using a set
+function arrayIntersection(arr1, arr2) {
+    const set1 = new Set(arr1);
+    const intersectionArr = [];
+
+    for (let num of arr2) {
+        if (set1.has(num)) {
+            intersectionArr.push(num);
+        }
+    }
+
+    return intersectionArr;
+};
 
 module.exports = arrayIntersection;
