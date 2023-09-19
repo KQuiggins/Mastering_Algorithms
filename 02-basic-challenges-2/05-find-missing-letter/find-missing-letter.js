31b1f53a7206e6c1bc0122e006d795a1f1219cf0
KQuiggins <kashfamilyvalues@gmail.com>
2023-09-19 15:39:08 -0400

    solved find missing letter

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,17 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    const startIndex = alphabet.indexOf(arr[0]);
+
+    for (let idx = 0; idx < arr.length; idx++) {
+        if (arr[idx] !== alphabet[startIndex + idx]) {
+            return alphabet[startIndex + idx]
+        }
+
+    }
+    return '';
+
+
+};
 
 module.exports = findMissingLetter;
