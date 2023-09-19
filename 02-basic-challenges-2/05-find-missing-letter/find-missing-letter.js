c17203915c2918acf473f3de9d1785bcf40ed2f5
KQuiggins <kashfamilyvalues@gmail.com>
2023-09-19 15:39:59 -0400

    solved find missing letter (charCodeAt)

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,17 +1,36 @@
 function findMissingLetter(arr) {
     const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-    const startIndex = alphabet.indexOf(arr[0]);
+    let start = arr[0].charCodeAt(0);
 
     for (let idx = 0; idx < arr.length; idx++) {
-        if (arr[idx] !== alphabet[startIndex + idx]) {
-            return alphabet[startIndex + idx]
-        }
+       const current = arr[idx].charCodeAt(0);
+
+       if (current - start > 1) {
+        return String.fromCharCode(start + 1);
+       }
+
+       start = current;
 
     }
     return '';
 
 
 };
+// function findMissingLetter(arr) {
+//     const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+//     const startIndex = alphabet.indexOf(arr[0]);
+
+//     for (let idx = 0; idx < arr.length; idx++) {
+//         if (arr[idx] !== alphabet[startIndex + idx]) {
+//             return alphabet[startIndex + idx]
+//         }
+
+//     }
+//     return '';
+
+
+// };
 
 module.exports = findMissingLetter;
