012cfa4714114ff0920f294de41131b848aafbbf
KQuiggins <kashfamilyvalues@gmail.com>
2023-09-21 07:21:20 -0400

    solved are all characters unique challenge

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,39 @@
-function areAllCharactersUnique() {}
+// Using objects
+
+// function areAllCharactersUnique(str) {
+//     const charCount = {};
+
+//     for (let idx = 0; idx < str.length; idx++) {
+//         const char = str[idx];
+
+//         if (charCount[char]) {
+//             return false;
+//         }
+
+//         charCount[char] = true;
+
+//     }
+
+//     return true;
+// };
+
+// Using sets
+
+function areAllCharactersUnique(str) {
+    const charSet = new Set();
+
+    for (let idx = 0; idx < str.length; idx++) {
+        const char = str[idx];
+
+        if (charSet.has(char)) {
+            return false;
+        }
+
+        charSet.add(char);
+
+    }
+
+    return true;
+};
 
 module.exports = areAllCharactersUnique;
