04e4c604dc2373da14e84f6811f22d259553901e
KQuiggins <kashfamilyvalues@gmail.com>
2023-09-26 17:08:20 -0400

    solved dice game challenge

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,39 @@
-function diceGameSimulation() {}
+function RollDice() {
+    return Math.floor(Math.random() * 6) + 1;
+
+}
+
+
+function diceGameSimulation(numSimulations) {
+
+
+    let results = [];
+
+    for (let idx = 0; idx < numSimulations; idx++) {
+        const dice1 = RollDice();
+        const dice2 = RollDice();
+        let sum = dice1 + dice2;
+
+        let result = '';
+
+        if (sum === 7 || sum === 11) {
+            result = 'win';
+        } else if (sum === 2 || sum === 3 || sum === 12) {
+            result = 'lose';
+        } else {
+            result = 'roll again';
+        }
+
+        results.push({
+            dice1,
+            dice2,
+            sum,
+            result
+        });
+
+    }
+    return results;
+
+}
 
 module.exports = diceGameSimulation;
