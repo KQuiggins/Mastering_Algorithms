f6a27799bd1d1ac63ca308f604ffdeaa01e5a73c
KQuiggins <kashfamilyvalues@gmail.com>
2023-10-02 12:50:47 -0400

    solved validate email address challenge

--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,3 +1,27 @@
-function validateEmail() {}
+function validateEmail(email) {
+    if (email.indexOf('@') === -1) {
+        return false;
+    }
+
+    const [localPart, domain] = email.split('@');
+
+    if (localPart.length === 0 || domain.length < 3) {
+        return false;
+    }
+
+    const domainExtension = domain.split('.');
+
+    if (domainExtension.length < 2 || domainExtension[1] < 2) {
+        return false;
+    }
+
+    return true;
+}
+
+
+// function validateEmail(email) {
+//     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//     return regex.test(email);
+// }
 
 module.exports = validateEmail;
