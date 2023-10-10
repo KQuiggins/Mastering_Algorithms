7979432a09ec3ea07717fe53ddae1bf37103cec8
KQuiggins <kashfamilyvalues@gmail.com>
2023-10-10 16:03:27 -0400

    solved sum of even squares challenge

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,23 @@
-function sumOfEvenSquares() {}
+// function sumOfEvenSquares(numbers) {
+
+//     const evenSquares = numbers.filter((num) => num % 2 === 0)
+//         .map((num) => num ** 2)
+//         .reduce((acc, num) => acc + num, 0);
+
+//     return evenSquares;
+// }
+
+function sumOfEvenSquares(numbers) {
+    let evenSquares = 0;
+
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] % 2 === 0) {
+            evenSquares += numbers[i] ** 2;
+        }
+    };
+
+
+    return evenSquares;
+}
 
 module.exports = sumOfEvenSquares;
