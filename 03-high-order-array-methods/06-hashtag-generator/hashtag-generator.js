9627eedc2e2dd3b29bb1359b9f8ebdd8b1e2a2fb
KQuiggins <kashfamilyvalues@gmail.com>
2023-10-29 21:13:32 -0400

    solved hashtag generator problem

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,26 @@
-function generateHashtag() {}
+function generateHashtag(str) {
+    const hashtag = str.split(' ').reduce(function (tag, word) {
+        return tag + word.charAt(0).toUpperCase() + word.slice(1)
+    }, '#')
+
+    return hashtag.length === 1 || hashtag.length > 140 ? false : hashtag;
+}
+
+
+// function generateHashtag(str) {
+//     if (str.trim() === '') {
+//         return false;
+//     }
+
+//     const words = str.trim().split(/\s+/);
+
+//     const capitalizedWords = words.map((word) => {
+//         return word[0].toUpperCase() + word.slice(1);
+//     });
+
+//     const hashtag = '#' + capitalizedWords.join('');
+
+//     return hashtag.length > 140 ? false : hashtag;
+// }
 
 module.exports = generateHashtag;
