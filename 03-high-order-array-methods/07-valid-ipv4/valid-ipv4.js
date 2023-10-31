d760ee763ea5ab2418a596fd9ea656cd4dec6c7e
KQuiggins <kashfamilyvalues@gmail.com>
2023-10-31 16:17:37 -0400

    solved valid ipv4 problem

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,12 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (ip) => {
+    const ipArray = ip.split('.');
+    if (ipArray.length !== 4) return false;
+    return ipArray.every((num) => {
+        if (num.length > 1 && num[0] === '0') return false;
+        if (num.length > 3) return false;
+        if (num < 0 || num > 255) return false;
+        return true;
+    });
+};
 
 module.exports = isValidIPv4;
