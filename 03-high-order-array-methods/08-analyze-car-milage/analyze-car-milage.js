0c3640c8a4e030d5241377a1caef9dae04c127ac
KQuiggins <kashfamilyvalues@gmail.com>
2023-11-01 20:51:25 -0400

    solved analyze car mileage problem

--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,30 @@
-function analyzeCarMileage() {}
+function analyzeCarMileage(cars) {
+    // calculate the average mileage of all cars
+    // find car with highest mileage
+    // find car with lowest mileage
+    // total mileage of all cars combine
+
+    const totalMileage = cars.reduce((sum, car) => {
+        return sum + car.mileage;
+    }, 0);
+
+    const averageMileage = totalMileage / cars.length;
+
+    const highestMileageCar = cars.reduce((highest, car) => (car.mileage > highest.mileage ? car : highest), cars[0])
+
+    const lowestMileageCar = cars.reduce((lowest, car) => (car.mileage < lowest.mileage ? car : lowest), cars[0])
+
+
+
+    return {
+        averageMileage: parseFloat(averageMileage.toFixed(2)),
+        highestMileageCar,
+        lowestMileageCar,
+        totalMileage
+
+    };
+}
+
+
 
 module.exports = analyzeCarMileage;
