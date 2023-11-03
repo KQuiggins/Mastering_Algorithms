d26c0a3a01a486be616bad2f5f38a14b1f8f391f
KQuiggins <kashfamilyvalues@gmail.com>
2023-11-03 08:58:35 -0400

    solved password validator problem

--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,3 +1,22 @@
-function validatePassword() {}
+// Password must be 8 characters long
+// Must contain at least 1 uppercase letter
+// Must contain at least 1 lowercase letter
+// Must contain at least 1 digit
+// return true if valid, false otherwise
+
+function validatePassword(password) {
+
+    const passwordArr = password.split('');
+
+    const isLengthValid = passwordArr.length >= 8;
+
+    const hasUpperCase = passwordArr.some((char) => char === char.toUpperCase() && char !== char.toLowerCase());
+
+    const hasLowerCase = passwordArr.some((char) => char === char.toLowerCase() && char !== char.toUpperCase());
+
+    const hasDigit = passwordArr.some((char) => !isNaN(parseInt(char, 10)));
+
+    return isLengthValid && hasUpperCase && hasLowerCase && hasDigit;
+}
 
 module.exports = validatePassword;
