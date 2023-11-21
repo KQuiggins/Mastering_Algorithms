65bae5053efc449254213e5d994a802f084a266b
KQuiggins <kashfamilyvalues@gmail.com>
2023-11-21 07:51:37 -0500

    Add factorial function and test cases

--- 04-recursion/05-factorial/factorial.js
+++ 04-recursion/05-factorial/factorial.js
@@ -1,3 +1,10 @@
-function factorial() {}
+function factorial(num) {
+    if (num === 0 || num === 1) {
+        return 1;
+    };
+
+
+    return num * factorial(num - 1);
+}
 
 module.exports = factorial;
