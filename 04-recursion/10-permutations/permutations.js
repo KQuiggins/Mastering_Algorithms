9453fc291ef5fb329944db227b876c8d526ce27d
KQuiggins <kashfamilyvalues@gmail.com>
2023-12-02 08:14:28 -0500

    Add permutations function and test cases

--- 04-recursion/10-permutations/permutations.js
+++ 04-recursion/10-permutations/permutations.js
@@ -1,3 +1,22 @@
-function permutations() {}
+function permutations(str) {
+    let result = [];
+
+    if (str.length === 0) {
+        result.push('');
+        return result;
+    }
+
+    for (let i = 0; i < str.length; i++) {
+        let firstChar = str[i];
+        let remainingChars = str.slice(0, i) + str.slice(i + 1);
+        let innerPermutations = permutations(remainingChars);
+
+        for (let j = 0; j < innerPermutations.length; j++) {
+            result.push(firstChar + innerPermutations[j]);
+        }
+    }
+
+    return result;
+}
 
 module.exports = permutations;
