f809d5e1f8de472139b61e7ff0593d133403a7c5
KQuiggins <kashfamilyvalues@gmail.com>
2023-12-03 06:56:23 -0500

    Add constant time and max subarray quadratic functions

--- 05-complexity/03-constant-time-complexity/constant-time.js
+++ 05-complexity/03-constant-time-complexity/constant-time.js
@@ -1,5 +1,17 @@
+const { log, error, time, timeEnd } = require('console');
 /*
   Example of constant time O(1)
 
   Constant time means that the time required to complete a function is the same regardless of the size of the input data set.
 */
+
+// Example 1
+function constantTime1(array) {
+  return array[0];
+}
+
+let array = [1, 2, 3, 4, 5];
+
+time('constantTime1');
+log(constantTime1(array));
+timeEnd('constantTime1');
