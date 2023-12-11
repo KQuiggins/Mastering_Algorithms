0eda008446e6e9c3d06f0344576deb94303f6e7c
KQuiggins <kashfamilyvalues@gmail.com>
2023-12-11 13:53:03 -0500

    Commented out console.log statement in phone-number-directory-run.js

--- 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
+++ 06-hash-tables-maps-sets/05-phone-number-directory/phone-number-directory.js
@@ -1,3 +1,24 @@
-function phoneNumberDirectory() {}
+function phoneNumberDirectory(phoneNumbers) {
+    let phoneMap = new Map();
+
+
+
+
+
+    phoneNumbers.forEach(element => {
+        let splitEl = element.split(':');
+        //console.log(splitEl[0]);
+
+        const names = splitEl[0];
+        const numbers = splitEl[1];
+
+        phoneMap.set(names, numbers);
+    });
+
+    console.log(phoneMap);
+
+    return phoneMap;
+
+}
 
 module.exports = phoneNumberDirectory;
