5a8d4d3aee526d4c0191b22641886496c54d8865
KQuiggins <kashfamilyvalues@gmail.com>
2023-12-18 09:32:41 -0500

    Add twoSum function and test cases

--- 06-hash-tables-maps-sets/09-two-sum/two-sum.js
+++ 06-hash-tables-maps-sets/09-two-sum/two-sum.js
@@ -1,3 +1,19 @@
-function twoSum() {}
+function twoSum(nums, target) {
+    const numberSet = new Set;
+
+
+
+    for (let idx = 0; idx < nums.length; idx++) {
+        const complement = target - nums[idx];
+        
+        if (numberSet.has(complement)) {
+            return [nums.indexOf(complement), idx]
+        }
+
+        numberSet.add(nums[idx])
+    }
+
+    return []
+}
 
 module.exports = twoSum;
