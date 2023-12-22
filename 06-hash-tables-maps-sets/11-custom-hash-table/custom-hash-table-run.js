8754e104e9b41476312cbc012d897541aac9a8cc
KQuiggins <kashfamilyvalues@gmail.com>
2023-12-22 16:33:21 -0500

    Add custom hash table implementation and test

--- 06-hash-tables-maps-sets/11-custom-hash-table/custom-hash-table-run.js
+++ 06-hash-tables-maps-sets/11-custom-hash-table/custom-hash-table-run.js
@@ -1,1 +1,9 @@
 const HashTable = require('./custom-hash-table');
+
+const myHashTable = new HashTable()
+
+myHashTable.set('Kenny', '502-528-7390')
+myHashTable.set('Ashley', '502-938-5826')
+myHashTable.set('Kenzie', '502-650-7533')
+
+myHashTable.printTable()
--- 06-hash-tables-maps-sets/11-custom-hash-table/custom-hash-table.js
+++ 06-hash-tables-maps-sets/11-custom-hash-table/custom-hash-table.js
@@ -1,3 +1,51 @@
-class HashTable {}
+class HashTable {
+    constructor(limit = 14) {
+        this.storage = []
+        this.limit = limit
+    }
+
+    _hash(key, max) {
+        let hash = 0
+
+        for (let idx = 0; idx < key.length; idx++) {
+            hash += key.charCodeAt(idx)
+
+        }
+
+        return hash % max
+    }
+
+    printTable() {
+        for (let idx = 0; idx < this.storage.length; idx++) {
+            if (this.storage[idx] !== undefined) {
+                console.log(`Bucket ${idx}: ${JSON.stringify(this.storage[idx])}`);
+            } else {
+                console.log(`Bucket ${idx} Empty`);
+            }
+
+        }
+    }
+
+    set(key, value) {
+        const index = this._hash(key, this.limit)
+
+        if (this.storage[index] === undefined) {
+            this.storage[index] = [[key, value]]
+        } else {
+            let inserted = false
+
+            for (let idx = 0; idx < this.storage[index].length; idx++) {
+                if (this.storage[index][idx][0] === key) {
+                    this.storage[index][idx][1] = value
+                    inserted = true
+                }
+            }
+
+            if (inserted === false) {
+                this.storage[index].push([key, value])
+            }
+        }
+    }
+}
 
 module.exports = HashTable;
