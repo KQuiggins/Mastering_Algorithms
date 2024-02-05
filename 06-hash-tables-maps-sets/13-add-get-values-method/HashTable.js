4a978a3aeac0ce10f737df714d86736daa599328
KQuiggins <kashfamilyvalues@gmail.com>
2024-02-05 07:33:42 -0500

    Add getValues() method to HashTable class

--- 06-hash-tables-maps-sets/13-add-get-values-method/HashTable.js
+++ 06-hash-tables-maps-sets/13-add-get-values-method/HashTable.js
@@ -97,6 +97,19 @@ class HashTable {
   }
 
  // ADD getValues() METHOD
+ getValues() {
+  const values = [];
+
+  for (let i = 0; i < this.storage.length; i++) {
+
+    if (this.storage[i]) {
+        for (const [key, value] of this.storage[i]) {
+          values.push(value);
+        }
+      }
+    }
+    return values;
+  }
 }
 
 module.exports = HashTable;
