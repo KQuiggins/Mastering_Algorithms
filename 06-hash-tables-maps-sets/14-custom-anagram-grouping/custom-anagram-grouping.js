6f576e18f5fd6814b2005ac3db6c7731a643f309
KQuiggins <kashfamilyvalues@gmail.com>
2024-02-07 07:28:19 -0500

    Add custom-anagram-grouping implementation and test

--- 06-hash-tables-maps-sets/14-custom-anagram-grouping/custom-anagram-grouping.js
+++ 06-hash-tables-maps-sets/14-custom-anagram-grouping/custom-anagram-grouping.js
@@ -1,5 +1,18 @@
 const HashTable = require('./HashTable');
 
-function anagramGrouping() {}
+function anagramGrouping(words) {
+    const hastTable = new HashTable();
+
+    for (const word of words) {
+        const sortedChars = word.split('').sort().join('');
+
+        if (hastTable.get(sortedChars)) {
+            hastTable.get(sortedChars).push(word);
+        } else {
+            hastTable.set(sortedChars, [word]);
+        }
+    }
+    return hastTable.getValues();
+}
 
 module.exports = anagramGrouping;
