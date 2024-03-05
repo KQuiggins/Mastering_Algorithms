884d9994f4da3bd6e2c7a7c0e56dd448d683f846
KQuiggins <kashfamilyvalues@gmail.com>
2024-03-05 05:28:57 -0500

    Add balancedParenthesis function and test cases, and implement Queue class

--- 07-stacks-queues-linked-lists/06-queue-implementation/queue.js
+++ 07-stacks-queues-linked-lists/06-queue-implementation/queue.js
@@ -1,3 +1,47 @@
-class Queue {}
+class Queue {
+    constructor() {
+        this.head = 0;
+        this.tail = 0;
+        this.queue = [];
+        this.maxSize = 100;
+    }
+    getLength() {
+        return this.tail - this.head;
+    }
+
+    isEmpty() {
+        return this.getLength() === 0;
+    }
+
+    isFull() {
+        return this.getLength() === this.maxSize;
+    }
+
+    enqueue(value) {
+        if (this.isFull()) {
+            return "Queue is empty";
+        }
+
+        this.queue[this.tail] = value;
+        this.tail++;
+        return true;
+    }
+
+    dequeue() {
+        if (this.isEmpty()) {
+            return "Queue is empty";
+        }
+        const value = this.queue[this.head];
+        this.head++;
+        return value;
+    }
+
+    peek() {
+        if (this.isEmpty()) {
+            return "Queue is empty";
+        }
+        return this.queue[this.head];
+    }
+}
 
 module.exports = Queue;
