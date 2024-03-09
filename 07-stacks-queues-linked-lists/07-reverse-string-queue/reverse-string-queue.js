3b6593116548aa3cec06dc07b7ddceb98b11644e
KQuiggins <kashfamilyvalues@gmail.com>
2024-03-09 08:54:02 -0500

    Add reverseStringWithQueue function and test cases

--- 07-stacks-queues-linked-lists/07-reverse-string-queue/reverse-string-queue.js
+++ 07-stacks-queues-linked-lists/07-reverse-string-queue/reverse-string-queue.js
@@ -1,5 +1,18 @@
 const Queue = require('./queue');
 
-const reverseStringWithQueue = (str) => {};
+const reverseStringWithQueue = (str) => {
+    const queue = new Queue();
+    let reversedString = '';
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        queue.enqueue(str[i]);
+    }
+
+    while (!queue.isEmpty()) {
+        reversedString += queue.dequeue();
+    }
+
+    return reversedString;
+};
 
 module.exports = reverseStringWithQueue;
