4cbbbf1b51b2bc9f32dd8645bf095cd06d0af608
KQuiggins <kashfamilyvalues@gmail.com>
2024-03-10 07:55:09 -0400

    Refactor palindrome checking function with queue and stack

--- 07-stacks-queues-linked-lists/08-palindrome-queue-stack.js/palindrome-queue-stack.js
+++ 07-stacks-queues-linked-lists/08-palindrome-queue-stack.js/palindrome-queue-stack.js
@@ -1,6 +1,27 @@
-const Queue = require('./queue');
-const Stack = require('./stack');
+const Queue = require("./queue");
+const Stack = require("./stack");
 
-function isPalindromeQueueStack() {}
+function isPalindromeQueueStack(str) {
+  const regex = /[^a-zA-Z0-9]/g;
+  const formattedStr = str.replace(regex, "").toLowerCase();
+
+  const stack = new Stack();
+  const queue = new Queue();
+
+  for (let i = 0; i < formattedStr.length; i++) {
+    const char = formattedStr.charAt(i);
+
+    queue.enqueue(char);
+    stack.push(char);
+  }
+
+  while (!queue.isEmpty()) {
+    if (queue.dequeue() !== stack.pop()) {
+        return false;
+    }
+  }
+
+  return true;
+}
 
 module.exports = isPalindromeQueueStack;
