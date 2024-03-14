297735bf80910b5d81bba329513f9ed0f3b841a1
KQuiggins <kashfamilyvalues@gmail.com>
2024-03-13 22:05:38 -0400

    Implement linked list methods and tests

--- 07-stacks-queues-linked-lists/10-linked-list-implementation/linked-list-run.js
+++ 07-stacks-queues-linked-lists/10-linked-list-implementation/linked-list-run.js
@@ -1,1 +1,16 @@
 const { LinkedList } = require('./linked-list');
+
+const list = new LinkedList();
+
+
+list.add(100);
+list.add(200);
+list.add(300);
+list.add(400);
+
+list.insertAt(1, 'ken');
+//list.removeFrom(1);
+
+//console.log(list.get(2));
+
+list.printAll();
