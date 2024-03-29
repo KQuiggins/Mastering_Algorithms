6c3f79f6c3d7c555d143d61a867291a48edd1806
KQuiggins <kashfamilyvalues@gmail.com>
2024-03-28 21:31:54 -0400

    Update doubly linked list implementation and add test cases

--- 07-stacks-queues-linked-lists/15-doubly-linked-list-implementation/doubly-linked-list-run.js
+++ 07-stacks-queues-linked-lists/15-doubly-linked-list-implementation/doubly-linked-list-run.js
@@ -2,10 +2,10 @@ const DoublyLinkedList = require('./doubly-linked-list');
 
 const list = new DoublyLinkedList();
 
-list.append(2);
-list.append(3);
-list.append(4);
-list.prepend(1);
+list.append(100);
+list.append(200);
+list.append(300);
+list.prepend(400);
 
 list.insertAt(2, "NEW");
 
