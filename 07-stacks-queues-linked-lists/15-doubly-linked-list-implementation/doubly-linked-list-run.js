8e1e7a657fcaba799f684d92034e883cc066fd08
KQuiggins <kashfamilyvalues@gmail.com>
2024-03-28 21:31:27 -0400

    Add doubly linked list implementation with append, prepend, insertAt, and printAll methods

--- 07-stacks-queues-linked-lists/15-doubly-linked-list-implementation/doubly-linked-list-run.js
+++ 07-stacks-queues-linked-lists/15-doubly-linked-list-implementation/doubly-linked-list-run.js
@@ -1,2 +1,13 @@
 const DoublyLinkedList = require('./doubly-linked-list');
 
+const list = new DoublyLinkedList();
+
+list.append(2);
+list.append(3);
+list.append(4);
+list.prepend(1);
+
+list.insertAt(2, "NEW");
+
+list.printAll();
+
