bb14d0c8eb2a9b96fa9828fa5406e540c9cc3daf
KQuiggins <kashfamilyvalues@gmail.com>
2024-04-06 13:12:52 -0400

    Add TreeNode class and test for manual binary tree

--- 08-binary-trees-graphs/02-tree-node-class/tree-node-class-run.js
+++ 08-binary-trees-graphs/02-tree-node-class/tree-node-class-run.js
@@ -1,1 +1,18 @@
 const TreeNode = require('./tree-node-class');
+
+const a = new TreeNode('A');
+const b = new TreeNode('B');
+const c = new TreeNode('C');
+const d = new TreeNode('D');
+const e = new TreeNode('E');
+const f = new TreeNode('F');
+
+a.left = b;
+a.right = c;
+
+b.left = d;
+b.right = e;
+
+c.right = f;
+
+console.log(a);
