1842409fd6f78c0d8173384d4da7e56c63b9002d
KQuiggins <kashfamilyvalues@gmail.com>
2024-05-10 16:46:25 -0400

    refactor: Update depth-first traversal to use recursive approach

--- 08-binary-trees-graphs/04-depth-first-traversal-recursive/depth-first-traversal-recursive.js
+++ 08-binary-trees-graphs/04-depth-first-traversal-recursive/depth-first-traversal-recursive.js
@@ -1,6 +1,25 @@
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
 
-function recDepthFirstTraversal() {}
+function recDepthFirstTraversal(root) {
+  const result = [];
+
+  function traverseNode(node) {
+    if (node !== null) {
+      result.push(node.data);
+      traverseNode(node.left);
+      traverseNode(node.right);
+    }
+  }
+
+  traverseNode(root);
+  return result;
+}
 
 module.exports = {
   Node,
